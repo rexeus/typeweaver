133364114c3a8c00d55c66ce22374dd3c15ac145
Dennis Wentzien <my+github@wentzien.net>
2025-07-26 15:17:23 +0200

    feat: prepare for client tests

--- packages/test-utils/src/test-server/createTestServer.ts
+++ packages/test-utils/src/test-server/createTestServer.ts
@@ -5,22 +5,45 @@ import { AuthHandlers } from "./handlers/AuthHandlers";
 import { AccountHandlers } from "./handlers/AccountApiHandler";
 import { SpecimenHandlers } from "./handlers/SpecimenHandlers";
 import { serve, type ServerType } from "@hono/node-server";
+import { HttpResponse } from "@rexeus/typeweaver-core";
+import getPort, { portNumbers } from "get-port";
 
-export async function createTestServer(port: number): Promise<ServerType> {
+export type CreateTestServerResult = {
+  readonly server: ServerType;
+  readonly baseUrl: string;
+};
+
+export async function createTestServer(handlerOptions?: {
+  readonly todoError?: Error | HttpResponse;
+  readonly authError?: Error | HttpResponse;
+  readonly accountError?: Error | HttpResponse;
+  readonly specimenError?: Error | HttpResponse;
+}): Promise<CreateTestServerResult> {
   const app = new Hono();
 
-  const todoRouter = new TodoHono(new TodoHandlers());
-  const authRouter = new AuthHono(new AuthHandlers());
-  const accountRouter = new AccountHono(new AccountHandlers());
-  const specimenRouter = new SpecimenHono(new SpecimenHandlers());
+  const todoRouter = new TodoHono(new TodoHandlers(handlerOptions?.todoError));
+  const authRouter = new AuthHono(new AuthHandlers(handlerOptions?.authError));
+  const accountRouter = new AccountHono(
+    new AccountHandlers(handlerOptions?.accountError)
+  );
+  const specimenRouter = new SpecimenHono(
+    new SpecimenHandlers(handlerOptions?.specimenError)
+  );
 
   app.route("/", authRouter);
   app.route("/", accountRouter);
   app.route("/", todoRouter);
   app.route("/", specimenRouter);
 
-  return serve({
+  const port = await getPort({ port: portNumbers(3000, 3100) });
+
+  const server = serve({
     fetch: app.fetch,
     port,
   });
+
+  return {
+    server,
+    baseUrl: `http://localhost:${port}`,
+  };
 }
